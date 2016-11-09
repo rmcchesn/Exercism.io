c2206b06df048ad0bb8d676eed253fa63bb281ad
Ryan McChesney <rmcchesn@gmail.com>
2016-11-09 13:01:24 -0800

    JS - leap: complete 2 tests

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,11 @@
 'use strict';
 
-let Year = function() { };
+let Year = function(year) {
+  this.year = year;
+};
 
-Year.prototype.isLeap = (year) =>  false; 
+Year.prototype.isLeap = function() {
+  return this.year % 4 === 0;
+};
 
 module.exports = Year;
