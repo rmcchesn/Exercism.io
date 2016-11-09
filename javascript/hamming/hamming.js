38968272ed532b13ba97928b91e0b344d863a0c8
Ryan McChesney <rmcchesn@gmail.com>
2016-11-09 13:52:44 -0800

    JS - hamming: solves all tests.

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -2,4 +2,19 @@
 
 let Hamming = function() {};
 
+Hamming.prototype.compute = function(strand1, strand2) {
+  let hammingDistance = 0;
+
+  if (strand1.length !== strand2.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
+
+  strand1.split('')
+    .forEach((nucleotide, index) => {
+      if (strand2[index] !== nucleotide) { hammingDistance += 1; }
+    });
+
+  return hammingDistance;
+};
+
 module.exports = Hamming;
