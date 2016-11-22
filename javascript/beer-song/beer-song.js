6f05c2c4d4c4aeeb80cb2038a5aecc4983a732ff
Ryan McChesney <rmcchesn@gmail.com>
2016-11-22 15:25:33 -0800

    JS - beersong: Complete first 3 tests. Utilizes multiple if / else if statements

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,7 +1,30 @@
+'use strict';
+
 let BeerSong = function() { };
 
-BeerSong.prototype.verse = function() {
+BeerSong.prototype.verse = function(verseNum) {
+  let firstSentence, secondSentence;
 
+  if (verseNum > 2) {
+    firstSentence = `${verseNum} bottles of beer on the wall, ` +
+      `${verseNum} bottles of beer.\n`;
+    secondSentence = 'Take one down and pass it around, ' +
+      `${verseNum - 1} bottles of beer on the wall.\n`;
+  } else if (verseNum === 1) {
+    firstSentence = `${verseNum} bottle of beer on the wall, ` +
+      `${verseNum} bottle of beer.\n`;
+    secondSentence = 'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n';
+  } else {
+    firstSentence = `${verseNum} bottles of beer on the wall, ` +
+      `${verseNum} bottles of beer.\n`;
+    secondSentence = 'Take one down and pass it around, ' +
+      `${verseNum - 1} bottle of beer on the wall.\n`;
+  }
+
+  // console.log(firstSentence + secondSentence);
+
+  return firstSentence + secondSentence;
 };
 
 BeerSong.prototype.sing = function() {
