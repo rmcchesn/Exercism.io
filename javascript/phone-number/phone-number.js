421dde039d3e1859ac6602e8a23ce0f8107bf946
Ryan McChesney <rmcchesn@gmail.com>
2016-11-23 11:36:22 -0800

    JS - phonenumber: setup basic phone formatting function. Return raw ph number successfully.

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,9 +1,24 @@
 'use strict';
 
-let PhoneNumber = function() {
+let validatePhone = function(input) {
+  let re = /\D/g;
+  let phoneNum = '0000000000';
 
+  let digits = input.replace(re, '');
+
+  if (digits.length === 10) {
+    phoneNum = digits;
+  }
+
+  return phoneNum;
 };
 
+let PhoneNumber = function(input) {
+  this.phone = validatePhone(input);
+};
 
+PhoneNumber.prototype.number = function() {
+  return this.phone;
+};
 
 module.exports = PhoneNumber;
