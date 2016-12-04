1bbd16040f1138fc9c8c7d5d0dd9f1b4967a8d4b
Ryan McChesney <rmcchesn@gmail.com>
2016-12-03 17:41:32 -0800

    JS - foodchain: Set up basic file and prototype methods skeletons.

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let FoodChain = function() { };
+
+let animals = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow'];
+
+FoodChain.prototype.verse = function(verseNumber) {
+
+};
+
+FoodChain.prototype.verses = function(start, end) {
+
+};
+
+module.exports = FoodChain;
